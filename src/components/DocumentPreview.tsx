30f4c79c363d55b335caed3f4888df3b7a4db825
TharunKiruthik <120109765+TharunKiruthik@users.noreply.github.com>
2026-07-15 12:33:06 +0530

    Fix PDF footer logo misalign: use plain PNG instead of filter-heavy SVG (html2canvas can't rasterize SVG filters/masks reliably)

--- src/components/DocumentPreview.tsx
+++ src/components/DocumentPreview.tsx
@@ -364,7 +364,7 @@ export default function DocumentPreview(props: DocumentPreviewProps) {
             <a href="https://quotegen.kreateup.in" target="_blank" rel="noopener noreferrer"
                style={{ display: "inline-flex", alignItems: "center", gap: 6, fontSize: "11.5px", lineHeight: 1, color: "#888", textDecoration: "none" }}>
               {/* eslint-disable-next-line @next/next/no-img-element */}
-              <img src="/brand/quotegen/QG_icon_SVG.svg" alt="" style={{ height: 16, width: 16, display: "inline-block", verticalAlign: "middle" }} />
+              <img src="/brand/quotegen/QG_fav_42X42_T.png" alt="" style={{ height: 16, width: 16, display: "inline-block", verticalAlign: "middle" }} />
               Made with <span style={{ color: theme, fontWeight: 600 }}>QuoteGen</span>
             </a>
           )}
